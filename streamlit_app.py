1896b92493e715f6b46505ee059cca03fee2cac5
dwadsa122 <161122733+dwadsa122@users.noreply.github.com>
2025-11-25 04:34:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Practice Tool (Streamlit with machine learning')
 
-st.info('This is my attempt to create a streamlit app with a machine learning model inside.')
+st.write('This is my attempt to create a streamlit app with a machine learning model inside.')
