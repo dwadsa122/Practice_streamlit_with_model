c24886e93c500bc1a411e2bfe7e6797f8ebb36fc
dwadsa122 <161122733+dwadsa122@users.noreply.github.com>
2025-11-25 05:16:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,15 @@ st.title('Practice Tool (Streamlit with machine learning')
 
 with st.expander("Data"):
   st.write('**Raw Data**')
+  
   df = pd.read_csv("crocodile_dataset.csv")
+  df1 = df.drop(columns = df[['Observation ID','Scientific Name','Genus','Genus','Conservation Status', 'Observer Name', 'Notes','Family','Date of Observation']])
+  
   st.write("It is important to note that there are various classifications in the common name column, lets remove some columns that seem to be unnecessary, and we'll only keep those that we need.")
   st.write("Below shall be the X values with all of its features")
   X = df.drop(columns = df[['Observation ID','Scientific Name','Genus','Genus','Conservation Status', 'Observer Name', 'Notes','Family','Date of Observation', 'Common Name']])
   X
+  
   st.write("And here shall be our class target")
   y = df['Common Name']
   y
