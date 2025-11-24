9419e6d03c023faf6c7d23d51796305cda7d1095
dwadsa122 <161122733+dwadsa122@users.noreply.github.com>
2025-11-25 05:30:32 +0800

    Add seaborn import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title('Practice Tool (Streamlit with machine learning')
 
