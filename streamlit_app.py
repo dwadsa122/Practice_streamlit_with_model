28c61f4fbcc197fc40d74538ba38bb1d59617a54
dwadsa122 <161122733+dwadsa122@users.noreply.github.com>
2025-11-25 04:44:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('Practice Tool (Streamlit with machine learning')
 
 st.write('This is my attempt to create a streamlit app with a machine learning model inside.')
+
+with st.expander("Data"):
+  st.write('**Raw Data**')
+  df = pd.read_csv("crocodile_dataset.csv")
+  df
